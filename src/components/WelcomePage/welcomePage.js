e67db27f8dab1bc1e5f5fd44071dfb9df588e0d1
Humay <crypto347@gmail.com>
2019-07-06 13:57:31 +0400

    refactoring code

--- src/components/WelcomePage/welcomePage.js
+++ src/components/WelcomePage/welcomePage.js
@@ -50,22 +50,22 @@ import {
  /**
   *  Post component definition and export
   */
- function WelcomePage (props1) {
+ function WelcomePage (props) {
    const calc = (x, y) => [-(y - window.innerHeight / 2) / 20, (x - window.innerWidth / 2) / 20, 1.1]
    const trans = (x, y, s) => `perspective(600px) rotateX(${x}deg) rotateY(${y}deg) scale(${s})`
-   const [props, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } }))
+   const [props1, set] = useSpring(() => ({ xys: [0, 0, 1], config: { mass: 5, tension: 350, friction: 40 } }))
    return(
    <div className="welcomePage">
           <animated.div
                className="welcomeWord"
                onMouseMove={({ clientX: x, clientY: y }) => set({ xys: calc(x, y) })}
                onMouseLeave={() => set({ xys: [0, 0, 1] })}
-               style={{ transform: props.xys.interpolate(trans) }}
+               style={{ transform: props1.xys.interpolate(trans) }}
             >
                Welcome
             </animated.div>
             <div className="horizontalDiv"/>
-         <NavLink className="linkHome" to={{ pathname: props1.match.url + 'Home'}}>Press to visit home page</NavLink>
+         <NavLink className="linkHome" to={{ pathname: props.match.url + 'Home'}}>Press to visit home page</NavLink>
    </div>
    );
     
