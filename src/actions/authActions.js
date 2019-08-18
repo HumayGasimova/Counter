cc59804fd9a5ed7468bf20295fe934e8e1ec84df
Humay <crypto347@gmail.com>
2019-08-18 13:11:20 +0400

    Log Out (delete token)

--- src/actions/authActions.js
+++ src/actions/authActions.js
@@ -4,7 +4,7 @@ import axios from 'axios';
 export function authStart() {
     return { 
         type: actionTypes.AUTH_START
-    }
+    };
 };
 
 export function authSuccess(token, userId) {
@@ -12,15 +12,29 @@ export function authSuccess(token, userId) {
         type: actionTypes.AUTH_SUCCESS, 
         idToken: token,
         userId: userId
-    }
+    };
 };
 
 export function authFail(error) {
     return { 
         type: actionTypes.AUTH_FAIL, 
         error: error
-    }
+    };
 };
+
+export const logout = () => {
+    return {
+        type: actionTypes.AUTH_LOGOUT
+    };
+}
+
+export const checkAuthTimeout = (expirationTime) => {
+    return dispatch => {
+        setTimeout(() => {
+            dispatch(logout());
+        }, expirationTime * 1000)
+    };
+}
 
 export function auth(email, password, isSignup) {
     return dispatch => {
@@ -34,11 +48,11 @@ export function auth(email, password, isSignup) {
         if(!isSignup){
             url = "https://identitytoolkit.googleapis.com/v1/accounts:signInWithPassword?key=AIzaSyCGc5wN44Lg_XZPdU-c34KhMBCIi-TS1uc";
         }
-        console.log(url)
         axios.post(url, authData)
         .then(response => {
             console.log(response);
             dispatch(authSuccess(response.data.idToken, response.data.localId));
+            dispatch(checkAuthTimeout(response.data.expiresIn))
         })
         .catch(err => {
             console.log(err.response);
